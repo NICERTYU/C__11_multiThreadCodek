a78de31ef6b45b912a39ec36e50c48b9bb6e4fce
NICERTYU <148675863+NICERTYU@users.noreply.github.com>
2024-02-29 23:12:08 +0800

    initl thread use

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include<thread>
+//create a thread function for thread
+void Hi() {
+    std::cout<<"klohshjj";
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    //pass the pointer of function
+    std::thread the(Hi);
+    the.join();
     return 0;
 }
